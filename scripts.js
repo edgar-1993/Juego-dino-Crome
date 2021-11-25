4b8798266c4d048d196ae69ed14b4a28918c22a2
edgar espinola <edgar.espinoladavid@gmail.com>
2021-11-25 18:51:00 -0300

    juego dino crome

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const player = document.getElementById("player");
+
+document.addEventListener("click", function () {
+    player.classList.add("playerJump")
+});
+player.addEventListener('animationend', () => {
+    player.classList.remove("playerJump");
+});
+
+let score = 0;
+
+setInterval(() => {
+    score++;
+    document.getElementById("score").innerHTML = score
+}, 1000);
